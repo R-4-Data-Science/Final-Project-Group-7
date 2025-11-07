53767a719dbfcedb83fa5a0311c9af9119ed666b
Nimra Ismail <nii0002@auburn.edu>
2025-11-07 12:56:50 -0600

    Created minimum structure i.e Scope

---
+++ R/MultiPath/MultiPathForward.R
@@ -0,0 +1,11 @@
+#' Multi-path forward selection
+#'
+#' @param data Input data frame
+#' @param response Name of response variable
+#' @return List containing selected model
+#' @export
+multi_path_forward <- function(data, response) {
+  # your full algorithm goes here
+}
+
+devtools::document()
