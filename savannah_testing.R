a06d0450083a821e2826d4e1388339783df33fab
ser0126 <ser0126@auburn.edu>
2025-11-10 11:32:33 -0600

    write some notes

---
+++ savannah_testing.R
@@ -0,0 +1,16 @@
+# For building models section
+
+?detect_model_type
+# where did this function come from?
+
+?setdiff
+# the condition in part 6 of building models I don't think is met, does the loop
+# stop if the aic for children minimum decreases from parent minimum by a
+# certain epsilon amount
+
+# does it only keep the best L models, or does it keep L models of total
+# possible resulting models, like in part 7?
+
+
+
+?min
